f1f739945cf77e9a36b6f01ffd15c1c4926ba62d
Emmanuel Oduor <wyllademanu003@gmail.com>
2026-06-09 14:51:03 +0300

    Remove crossorigin from script/link tags, add inline error catcher
    - Add Vite plugin to strip crossorigin attributes from built HTML
    - Add window.onerror handler in index.html to display JS errors
      on screen instead of blank page
    - Remove crossorigin config attempt (handled by plugin now)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html.replace(/\s+crossorigin(=["'][^"']*["'])?/g, '')
+      }
+    }
+  ],
   base: '/static/',
   build: {
     outDir: '../app/static',
